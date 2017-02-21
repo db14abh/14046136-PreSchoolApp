24d91f75818f83987f1c2ef628b6f035f4aa99f0
db14abh <db14abh@herts.ac.uk>
2017-02-21 16:50:40 +0000

    Add files via upload
    Version 2 with calculations working and apples moving

---
+++ coursework v2/Dragging_Image/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dragging_Image
+//
+//  Created by Xianhui Che on 26/05/2016.
+//  Copyright © 2016 Xianhui Che. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
