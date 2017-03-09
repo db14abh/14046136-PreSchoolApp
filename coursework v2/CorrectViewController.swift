7666988abfd9caf7c0bfe3f6eee9af023a885c58
db14abh <db14abh@herts.ac.uk>
2017-03-09 19:51:19 +0000

    Add files via upload
    Added more animation and also clapping sound affect

--- coursework v2/CorrectViewController.swift
+++ coursework v2/CorrectViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CorrectViewController: UIViewController {
 
@@ -14,9 +15,61 @@ class CorrectViewController: UIViewController {
         performSegue(withIdentifier: "Main", sender: self)
         
     }
+    
+    @IBOutlet weak var thumbs_up: UIImageView!
+    
+    
+    @IBOutlet weak var taponscreen: UIImageView!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*func GifImages(){
+        let gifData = try? Data(contentsOf: Bundle.main.url(forResource: "celebrations", withExtension: "gif")!)
+        let gif = UIImage.gifWithData(data: gifData! as NSData)
+        let imageView = UIImageView(image: gif)
+        imageView.frame = CGRect(x: 0.0, y:0.0, width: 154.0, height: 128.0)
+        view.addSubview(imageView)
+        
+            }
+*/
+    
+    
+    //clapping sound 
+    var csMusic = AVAudioPlayer()
+    func clappingsound(){
+    do {
+        csMusic = try AVAudioPlayer (contentsOf: URL(fileURLWithPath: Bundle.main.path(forResource:"applause6", ofType:"mp3")!))
+        csMusic.prepareToPlay()
+    }
+    catch {
+    print("ClappingSound")
+    }
+    csMusic.play()
+    }
+    
+    
+    
+    
+    
+    func ThumbAnimation() {
+        thumbs_up.center.y = thumbs_up.center.y - 10
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseIn, .allowUserInteraction, .repeat, .autoreverse], animations: {
+            self.thumbs_up.center.y = self.thumbs_up.center.x + 10
+        }
+        , completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        clappingsound()
+        //GifImages()
         // Do any additional setup after loading the view.
     }
 
@@ -25,11 +78,22 @@ class CorrectViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidAppear(_ _animated: Bool) {
+        super.viewDidAppear(_animated)
+        FadeAnimation()
+        ThumbAnimation()
+    }
     
     
-    
-    
-    
+    func FadeAnimation(){
+        UIView.animate (withDuration: 0.5, delay: 0, options: [.repeat, .autoreverse], animations: {
+            self.taponscreen.alpha = 0
+        }, completion:nil)
+        
+        }
+    }
+
+
     
 
     /*
@@ -42,4 +106,5 @@ class CorrectViewController: UIViewController {
     }
     */
 
-}
+
+
